d5f06b8638723dcad0b2fa56e2a203ae8096bd75
DigMz <diegomtz1080@gmail.com>
2026-06-20 12:21:54 -0500

    Nvim supertabs maybe 4

--- home/lazyvim/lazyvim-config/plugins/supertab.lua
+++ home/lazyvim/lazyvim-config/plugins/supertab.lua
@@ -3,8 +3,8 @@ return {
     "saghen/blink.cmp",
     opts = {
       keymap = {
-        ['<Tab>'] = { 'snippet_forward', 'fallback' },
-        ['S-<Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+        ['S-<Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
         ['<CR>'] = { 'accept', 'fallback' },
       },
     },
