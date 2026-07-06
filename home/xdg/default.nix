3583126c8b048d757b5ccfffb121154654a8853b
DigMz <diegomtz1080@gmail.com>
2026-07-05 21:57:47 -0500

    Desktop Entry for Wallpaper

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -3,28 +3,8 @@
 {
   xdg = {
     desktopEntries = {
-
-      firefox = {
-        categories = [
-          "Network"
-          "WebBrowser"
-        ];
-        exec = "firefox %U";
-        genericName = "Web Browser";
-        mimeType = [
-          "text/html"
-          "text/xml"
-        ];
-        name = "Firefox";
-        terminal = false;
-      };
-
       wallpaper_launcher = {
-        categories = [
-          "Wallpaper"
-          "Launcher"
-        ];
-        exec = "~/dotfiles/nixos/home/rofi/wallpaper_launcher/wallpaper-launcher.sh";
+        exec = "/home/digmz/dotfiles/nixos/home/rofi/wallpaper_launcher/wallpaper-launcher.sh";
         genericName = "wallpaper-launcher";
         name = "Wallpaper Launcher";
         terminal = false;
