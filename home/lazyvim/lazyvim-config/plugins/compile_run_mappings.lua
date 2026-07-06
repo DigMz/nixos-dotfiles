27bea0a2db8b2d9f1b5adad6e8eb83e9d2556622
DigMz <diegomtz1080@gmail.com>
2026-07-05 19:17:58 -0500

    Parity with old nvim config

---
+++ home/lazyvim/lazyvim-config/plugins/compile_run_mappings.lua
@@ -0,0 +1,26 @@
+local function open_terminal(cmd)
+  vim.cmd("w")
+  local width = math.floor(vim.o.columns * 0.9)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  })
+  -- vim.fn.termopen(cmd)
+  vim.fn.jobstart(cmd, { term = true })
+  vim.cmd("startinsert")
+end
+
+vim.keymap.set("n", "<F4>", function()
+  open_terminal("make")
+end, { desc = "Run make in floating terminal" })
+
+return {}
