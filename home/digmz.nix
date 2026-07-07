0ecac2cad00b538cc394094e9eb9bd4984df9987
DigMz <diegomtz1080@gmail.com>
2026-07-07 15:25:04 -0500

    Moved Rofi Hyprland Interplay to User Specific Functionality

--- home/digmz.nix
+++ home/digmz.nix
@@ -17,6 +17,18 @@
     ./lazyvim
   ];
 
+  xdg = {
+    desktopEntries = {
+      wallpaper_launcher = {
+        exec = "/home/digmz/dotfiles/nixos/home/rofi/wallpaper_launcher/wallpaper-launcher.sh";
+        genericName = "wallpaper-launcher";
+        name = "Wallpaper Launcher";
+        terminal = false;
+      };
+
+    };
+  };
+
   programs.bash = {
     enable = true;
     sessionVariables = {
--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -1,15 +1,4 @@
 { config, pkgs, ... }:
 
 {
-  xdg = {
-    desktopEntries = {
-      wallpaper_launcher = {
-        exec = "/home/digmz/dotfiles/nixos/home/rofi/wallpaper_launcher/wallpaper-launcher.sh";
-        genericName = "wallpaper-launcher";
-        name = "Wallpaper Launcher";
-        terminal = false;
-      };
-
-    };
-  };
 }
